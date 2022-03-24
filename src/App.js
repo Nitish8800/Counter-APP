611e8b2004cdbce263bccc7fd4fda4c52c5039be
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-03-24 18:32:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>inc</button>
+      <button
+        onClick={() => {
+          count === 0 ? setCount(0) : setCount(count - 1);
+        }}
+      >
+        dec
+      </button>
+    </div>
+  );
+}
